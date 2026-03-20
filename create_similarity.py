d2e815777d2c4a2c841d7f4c915143505631d309
Harsh Saini <sainiharsh6544@gmail.com>
2026-03-20 11:27:40 +0530

    Add files via upload

---
+++ create_similarity.py
@@ -0,0 +1,116 @@
+# create_similarity.py
+
+import pandas as pd
+import numpy as np
+import ast
+import pickle
+
+# Load datasets
+movies = pd.read_csv('tmdb_5000_movies.csv')
+credits = pd.read_csv('tmdb_5000_credits.csv')
+
+# Merge datasets
+movies = movies.merge(credits, on='title')
+
+movies = movies[['movie_id','title','overview','genres','keywords','cast','crew']]
+
+# Remove null values
+movies.dropna(inplace=True)
+
+# Remove duplicates
+movies.drop_duplicates(inplace=True)
+
+
+# Convert string dictionary to list
+def convert(obj):
+    l = []
+    for i in ast.literal_eval(obj):
+        l.append(i['name'])
+    return l
+
+
+movies['genres'] = movies['genres'].apply(convert)
+movies['keywords'] = movies['keywords'].apply(convert)
+
+
+# Top 3 actors
+def convert3(obj):
+    l=[]
+    counter=0
+    for i in ast.literal_eval(obj):
+        if counter != 3:
+            l.append(i['name'])
+            counter+=1
+        else:
+            break
+    return l
+
+
+movies['cast'] = movies['cast'].apply(convert3)
+
+
+# Fetch director
+def fetch_director(obj):
+    l=[]
+    for i in ast.literal_eval(obj):
+        if i['job'] == 'Director':
+            l.append(i['name'])
+            break
+    return l
+
+
+movies['crew'] = movies['crew'].apply(fetch_director)
+
+# Split overview
+movies['overview'] = movies['overview'].apply(lambda x: x.split())
+
+# Remove spaces
+movies['genres'] = movies['genres'].apply(lambda x:[i.replace(" ","") for i in x])
+movies['keywords'] = movies['keywords'].apply(lambda x:[i.replace(" ","") for i in x])
+movies['cast'] = movies['cast'].apply(lambda x:[i.replace(" ","") for i in x])
+movies['crew'] = movies['crew'].apply(lambda x:[i.replace(" ","") for i in x])
+
+# Tags
+movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
+
+new_df = movies[['movie_id','title','tags']].copy()
+
+# Convert list to string
+new_df['tags'] = new_df['tags'].apply(lambda x:" ".join(x))
+
+new_df['tags'] = new_df['tags'].apply(lambda x:x.lower())
+
+
+# Stemming
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+def stem(text):
+    y=[]
+    for i in text.split():
+        y.append(ps.stem(i))
+    return " ".join(y)
+
+new_df['tags'] = new_df['tags'].apply(stem)
+
+
+# Vectorization
+from sklearn.feature_extraction.text import CountVectorizer
+
+cv = CountVectorizer(max_features=5000, stop_words='english')
+
+vectors = cv.fit_transform(new_df['tags']).toarray()
+
+
+# Cosine similarity
+from sklearn.metrics.pairwise import cosine_similarity
+
+similarity = cosine_similarity(vectors)
+
+
+# Save files
+pickle.dump(new_df, open('movie_list.pkl','wb'))
+pickle.dump(similarity, open('similarity.pkl','wb'))
+
+print("Similarity file created successfully!")
