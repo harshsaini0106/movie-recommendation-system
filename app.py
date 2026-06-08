3c890876dfbeddb778abd1a41461d557388325d3
Harsh Saini <sainiharsh6544@gmail.com>
2026-06-08 18:51:28 +0530

    Fresh upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+<<<<<<< HEAD
 import streamlit as st
 import pickle
 import requests
@@ -81,17 +82,63 @@ def recommend(movie):
         reverse=True,
         key=lambda x: x[1]
     )[1:16]  # top 15 recommendations
+=======
+
+if not os.path.exists("similarity.pkl"):
+    import create_similarity
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+# Load saved data
+movies = pickle.load(open('movie_list.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+# Fetch movie poster from TMDB
+def fetch_poster(movie_id):
+
+    api_key = "282dfcf7bf624fd28b95dc075297fb5f"
+
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US"
+
+    data = requests.get(url)
+    data = data.json()
+
+    poster_path = data['poster_path']
+
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+
+    return full_path
+
+
+# Recommendation Function
+def recommend(movie):
+
+    movie_index = movies[movies['title'] == movie].index[0]
+
+    distances = similarity[movie_index]
+
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x:x[1])[1:16]
+>>>>>>> 110d015999e5cc3744a5cbbefcf45ca8433fa3a5
 
     recommended_movies = []
     recommended_posters = []
 
     for i in movies_list:
+<<<<<<< HEAD
         movie_id = movies.iloc[i[0]].movie_id
+=======
+
+        movie_id = movies.iloc[i[0]].movie_id
+
+>>>>>>> 110d015999e5cc3744a5cbbefcf45ca8433fa3a5
         recommended_movies.append(movies.iloc[i[0]].title)
         recommended_posters.append(fetch_poster(movie_id))
 
     return recommended_movies, recommended_posters
 
+<<<<<<< HEAD
 # UI Title
 st.markdown("<h1>🎬 Movie Recommendation System</h1>", unsafe_allow_html=True)
 st.markdown("### Find movies similar to your favourite ones 🍿")
@@ -119,3 +166,27 @@ if st.button("🎥 Show Recommendations"):
                             f"<div class='movie-title'>{names[i+j]}</div>",
                             unsafe_allow_html=True
                         )
+=======
+
+# Streamlit UI
+st.title("🎬 Movie Recommendation System")
+
+selected_movie = st.selectbox(
+    "Select a movie",
+    movies['title'].values
+)
+
+# Button
+if st.button("Recommend"):
+
+    names, posters = recommend(selected_movie)
+
+    for i in range(0, len(names), 5):
+        cols = st.columns(5)
+
+        for j in range(5):
+            if i + j < len(names):
+                with cols[j]:
+                    st.text(names[i + j])
+                    st.image(posters[i + j])
+>>>>>>> 110d015999e5cc3744a5cbbefcf45ca8433fa3a5
